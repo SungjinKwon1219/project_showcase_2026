469482c992b1a46077952df609907ea41dca0c80
KyleYu <56203835+KyleYuMusubi@users.noreply.github.com>
2026-04-27 16:00:06 -0700

    Update BACCalculator.py

--- BACCalculator.py
+++ BACCalculator.py
@@ -23,27 +23,26 @@ n_low = 0.1388081199
 n_mid = 0.0756061038
 
 #takes weight as kg and height as cm
-def r_coefficient(gender, age, weight, height, fat)
+def r_coefficient(gender, age, weight, height, fat):
       if gender == "m":
-            r_coefficient = m_i + m_age * age + m_weight * weight + m_height * height
+            r_val = m_i + m_age * age + m_weight * weight + m_height * height
             if fat == "low":
-                  r_coefficient += m_low
+                  r_val += m_low
             elif fat == "mid":
                   r_coefficient += m_mid
       elif gender == "f":
-            r_coefficient = f_i + f_age * age + f_weight * weight + f_height * height
+            r_val = f_i + f_age * age + f_weight * weight + f_height * height
             if fat == "low":
-                  r_coefficient += f_low
+                  r_val += f_low
             elif fat == "mid":
-                  r_coefficient += f_mid
+                  r_val += f_mid
       else:
-            r_coefficient = n_i + n_age * age + n_weight * weight + n_height * height
+            r_val = n_i + n_age * age + n_weight * weight + n_height * height
             if fat == "low":
-                  r_coefficient += n_low
+                  r_val += n_low
             elif fat == "mid":
-                  r_coefficient += n_mid
-      return r_coefficient
+                  r_val += n_mid
+      return r_val
 
 def BACCalculator(alc_g, weight, r, beta, time):
-    return (alc_g / (weight * r)) - beta * time
-
+    return (alc_g / (weight * 1000 * r)) - beta * time
